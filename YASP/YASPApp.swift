0597c9ba9da4ad09b69fd979002cd8cec8a1c51b
Til2ktyp <104679877+Til2ktyp@users.noreply.github.com>
2025-09-02 00:07:51 +0200

    XCODE ADDITION

---
+++ YASP/YASPApp.swift
@@ -0,0 +1,17 @@
+//
+//  YASPApp.swift
+//  YASP
+//
+//  Created by Til Freitag on 02.09.25.
+//
+
+import SwiftUI
+
+@main
+struct YASPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ YASPTests/YASPTests.swift
@@ -0,0 +1,16 @@
+//
+//  YASPTests.swift
+//  YASPTests
+//
+//  Created by Til Freitag on 02.09.25.
+//
+
+import Testing
+
+struct YASPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
